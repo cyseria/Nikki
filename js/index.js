11a3b3c4916f89ad1b82fa14c86ebc75d64249ee
cyseria <xcyseria@gmail.com>
2017-12-28 10:16:16 +0800

    修改布局使修正识别结果更方便

--- js/index.js
+++ js/index.js
@@ -1,36 +1,53 @@
 const filechooser = document.getElementById('filechooser');
 const preview = document.getElementById('preview');
 
-const clothesArr = []; // 衣服编号数组
+let imgPreview = []; // 图片预览 dom
+let clothesArr = []; // 衣服编号数组
+const picArr = []; // 图片处理相关 promise 函数
 const ocrArr = []; // 识别相关的 promise 函数
+
+// 数组容器
+let t = document.querySelector('#text'),
+    area = t.content.querySelector(".textarea");
 
 filechooser.onchange = function () {
     const files = this.files;
-    let promise = Promise.resolve();
-
     for (let i = 0; i < files.length; i++) {
         const file = files[i];
         if (!/\/(?:jpeg|jpg|png)/i.test(file.type)) { // 只接受 jpeg, jpg, png 类型的图片
             return;
         };
-        promise = promise
-            .then(() => { // 创建 FileReader
-                return createReader(file);
-            })
+
+        picArr.push(loadFile(file, i));
+    }
+
+    Promise.all(picArr)
+        .then(() => {
+            console.log('所有图片都处理完啦');
+        });
+
+};
+
+// 处理单个图片
+function loadFile(file, idx) {
+    return new Promise((resolve, reject) => {
+        createReader(file)
             .then(url => { // 加载图片
                 return loadImg(url)
             }).then(img => { // 裁剪图片
                 clipImg(img);
-
-                Promise.all(ocrArr).then(data => {
-                    console.log('check done');
-                    console.log(clothesArr);
-                });
+                Promise.all(ocrArr)
+                    .then(data => {
+                        console.log(`加载完第 ${idx} 张...`);
+                        area.value = clothesArr.join(', ');
+                        document.body.appendChild(area.cloneNode(true));
+                        document.body.appendChild(imgPreview[idx]);
+                        clothesArr = [];
+                        resolve();
+                    });
             })
-    }
-
-};
-
+    })
+}
 // 返回图片 data url
 function createReader(file) {
     return new Promise((resolve, reject) => {
@@ -55,12 +72,14 @@ function loadImg(url) {
 
 // 裁剪分割图片
 function clipImg(img) {
+    const ele = document.createElement('div');
     imageClipper(img, function () {
         const beginX = 240,
             beginY = 210, // 初始化开始裁剪的坐标
             offsetX = 310,
             offsetY = 178, // x, y 偏移量 
-            width = 70, // 被剪切图像的宽度
+
+            width = 50, // 被剪切图像的宽度
             height = 25; // 被剪切图像的高度
 
         let x, y; // 根据偏移计算后的裁剪坐标
@@ -70,18 +89,17 @@ function clipImg(img) {
             y = beginY + i * offsetY;
             for (let j = 0; j < 2; j++) {
                 x = beginX + j * offsetX;
-
                 this.reset()
                     .crop(x, y, width, height)
                     .toDataURL(dataUrl => { // 结果添加到 body
                         let myImage = new Image();
                         myImage.src = dataUrl;
-                        document.body.appendChild(myImage);
-
+                        ele.appendChild(myImage);
                         ocrArr.push(recoNum(dataUrl)); // 图像识别
                     });
             }
         }
+        imgPreview.push(ele)
     });
 }
 
@@ -89,17 +107,48 @@ function clipImg(img) {
 function recoNum(img) {
     return new Promise((resolve, reject) => {
         Tesseract.recognize(img, {
-                // lang: 'chi_sim',
-                // from_cache: true
+                tessedit_char_whitelist: ':/-0123456789',
+                // lang: 'chi_sim'
             })
             // .progress(function (message) {
             //     console.log('progress is: ', message)
             // })
             .then(function (result) {
-                console.log(result.text)
-                clothesArr.push(result.text);
+                // console.log(result.text)
+                const text = result.text.replace(/\r|\n/ig, "");
+                // const num = pad(parseInt(text), 3)
+                // console.log(`${typeof(num)}: ${num}`)
+                // 处理图片
+                clothesArr.push(text);
                 resolve();
             })
     })
 
 }
+
+function pad(num, n) {
+    var len = num.toString().length;
+    while (len < n) {
+        num = "0" + num;
+        len++;
+    }
+    return num;
+}
+
+const btnResult = document.getElementById('btn-result');
+btnResult.addEventListener('click', e => {
+    const textarea = document.querySelectorAll('.textarea');
+    let totalStr = '';
+    // 如果已经存在结果则直接修改 value
+    let resContain = document.querySelector('.nikki-contain');
+    if (!resContain) {
+        const res = document.querySelector('#nikki');
+        resContain = res.content.querySelector(".nikki-contain");
+    }
+    for (let i = 0; i < textarea.length; i++) {
+        totalStr += (textarea[i].value + '\n');
+    }
+
+    resContain.value = totalStr;
+    document.body.appendChild(resContain);
+})
